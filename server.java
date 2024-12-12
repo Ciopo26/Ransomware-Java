efdc6f4e773461d7bcad359f7f83b821b6d4e105
Ciopo26 <federico.cipolletti@itsrizzoli.it>
2024-12-12 10:39:38 +0100

    Add files via upload

---
+++ server.java
@@ -0,0 +1,44 @@
+package serverPazzo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+
+	public static void main(String[] args) {
+		String percorso = "C:\\Users\\FedericoCipolletti\\OneDrive - ITS Angelo Rizzoli\\Desktop\\soos.txt";
+		//inizializzazione server
+		try(ServerSocket socket = new ServerSocket(5000)){
+			System.out.println("Server Ora in ascolto sulla porta 5000");
+		
+			//accetta connessioni
+			while(true) {
+				Socket client = socket.accept();
+				System.out.println("Connessione accettata da " + client.getInetAddress());
+				
+				//ricezione dati
+				DataInputStream inputStream = new DataInputStream(client.getInputStream());
+	            String receivedMessage = inputStream.readUTF();
+	            System.out.println("Messaggio ricevuto: " + receivedMessage);
+	            
+	            //salvataggio in file di testo
+	            try (BufferedWriter writer = new BufferedWriter(new FileWriter(percorso, true))) {
+	                writer.write(receivedMessage+" IP: "+client.getInetAddress());
+	                writer.newLine(); // Aggiunge una nuova riga dopo il messaggio
+	                System.out.println("Messaggio salvato in: " + percorso);
+	            } catch (Exception e) {
+	                System.err.println("Errore durante la scrittura nel file: " + e.getMessage());
+	            }
+				
+			}			
+			
+		}catch(Exception e) {
+			System.out.println("Errore server");
+		}
+	}	
+}
